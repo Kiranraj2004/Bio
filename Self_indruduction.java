c1979ab080def3ea493c872ad0a9fc1f29a65753
Kiran Raj <139129621+Kiranraj2004@users.noreply.github.com>
2023-07-18 22:15:54 +0530

    Add files via upload

---
+++ Self_indruduction.java
@@ -0,0 +1,35 @@
+//Hello This is code of my intruduction in java programing language
+public class Self_indruduction{
+    public static void main(String[] args) {
+
+        System.out.println("\n Hi ! My name is Kiran Raj B ." +
+                "\n I was born in 2004 at Tumkuru district ,Karnataka ");
+        String year="Year";
+        String institution="Institution";
+
+        System.out.println("\n    EDUCATION " +
+                "\n    * Schooling " +
+                "\n       "    +year+" :  2010-2015 " +
+                "\n       "       +institution+": Matha English School  " +
+                "\n       "    +year+" : 2015-2020  "+
+                "\n       "       +institution+": Morarji Desai Residential School,adakimaranahalli,Bengaluru" +
+                "\n     * PUC  " +
+                "\n       "    +year+" : 2020-2022"+
+                "\n       "       +institution+" : Morarji Desai Residential PU College,Doddabadagere,Ramanagara" +
+                "\n     * BE.AIML  " +
+                "\n       "    +year+" : 2022-2026 "+
+                "\n       "       +institution+" :  New Horizon College of Enginnering,Marthahalli, Bengluru");
+
+        System.out.println();
+        System.out.println("    Hobbies " +
+                "\n          -> playing voleyball" +
+                "\n          -> Listening to music" +
+                "\n          -> Coding ");
+
+        System.out.println();
+        System.out.println("  Contact " +
+                "\n        -> Phone number : 9380056696" +
+                "\n        -> Email : kiranrajb5882@gmail.com");
+    }
+
+}
